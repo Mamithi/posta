1ebcd223bcc6b3827008a21c14419142fac2a07e
Mamithi <19lawrence93@gmail.com>
2017-05-12 19:24:25 +0300

    add data

---
+++ database/migrations/2017_05_12_192238_create_historys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistorysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('box');
+            $table->string('code');
+            $table->string('town');
+            $table->string('status');
+            $table->string('person_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('historys');
+    }
+}
