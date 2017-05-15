0544feb6948fa75770055a5563539ea1e3219824
Mamithi <19lawrence93@gmail.com>
2017-05-15 09:10:41 +0300

    import file

---
+++ database/migrations/2017_05_15_090914_create_views_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('views', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('box');
+            $table->string('name');
+            $table->string('code');
+            $table->string('town');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('views');
+    }
+}
